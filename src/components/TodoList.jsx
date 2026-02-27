22611bcfc22734703b948e24070bf84e0b964693
Daniel-bee <daniel123_86@live.com>
2026-02-27 12:12:35 +0300

    filter todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,12 @@
 import TodoItem from './TodoItem';
 
 const TodoList = ({ todos, setTodos }) => {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className="shadow-2xl w-1/2 mx-auto mt-6 p-10 space-y-4 divide-y-1 mb-20 divide-gray-200">
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <div key={item.name} className="py-2">
           <TodoItem item={item} todos={todos} setTodos={setTodos} />
         </div>
