8ad372a9df5558fd2b7cc166c69ef962348a7bf9
Daniel-bee <daniel123_86@live.com>
2026-02-27 07:36:47 +0300

    Display to do list

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react';
+import TodoItem from './components/TodoItem';
 
 const Todo = () => {
   const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           className="border"
           onChange={(e) => setTodo(e.target.value)}
           type="text"
           value={todo}
         />
-        <button type="submit">Add</button>
+        <button type="submit" className="border">
+          Add
+        </button>
+        {todos.map((item) => (
+          <TodoItem key={item} item={item} />
+        ))}
       </form>
     </>
   );
