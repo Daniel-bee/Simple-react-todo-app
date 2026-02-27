8d364ddf3ce5c037b838b72423d5010bab62f909
Daniel-bee <daniel123_86@live.com>
2026-02-27 08:22:11 +0300

    add and style header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from './components/Header';
 import Todo from './components/Todo';
 
 const App = () => {
   return (
     <>
+      <Header />
       <Todo />
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-yellow-300 text-center p-20">
+      <h2 className="text-3xl font-bold">My Todo List</h2>
+    </div>
+  );
+};
+
+export default Header;
